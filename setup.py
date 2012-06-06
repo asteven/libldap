bb09fddc3b85531e24fa482a4373da885e181499
Steven Armstrong <steven.armstrong@inf.ethz.ch>
2012-06-07 00:08:23 +0200

    define version in setup.py
    Signed-off-by: Steven Armstrong <steven.armstrong@inf.ethz.ch>

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
-from libldap import __version__
-
 name = 'libldap'
 
 setup(
     name=name,
-    version=__version__,
+    version='0.2.0',
     author='Steven Armstrong',
     author_email='steven-%s@armstrong.cc' % name,
     url='http://github.com/asteven/%s/' % name,
     description='Collection of helpers for working with ldap',
     packages=find_packages(),
-    install_requires=['distribute'],
+    install_requires=['distribute', 'python-ldap'],
 )
 
